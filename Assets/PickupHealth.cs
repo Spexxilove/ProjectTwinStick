316953ae556c0b272222a30d9568910ac8e3bf2b
SPEXX-PC_MK2\Spexxilove <steinacher@hotmail.com>
2017-06-12 01:18:49 +0200

    changed onEnemyDeath to event to make more on death effects possible added health pickup (doesn't drop yet)

---
+++ Assets/PickupHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupHealth : MonoBehaviour {
+
+	private float healAmount =1.0f;
+
+	void OnCollisionEnter2D(Collision2D collision){
+		GameObject other = collision.gameObject;
+		if (other.CompareTag ("Player")) {
+			Health health = other.GetComponent<Health> ();
+			if (health.getHealth () < health.maxHealth) {
+				health.heal (healAmount);
+				Destroy (gameObject);
+			}
+		}
+	}
+}
--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class EnemyDeath : MonoBehaviour
 {
 
+	public delegate void EnemyDeathEffectHandler(GameObject enemy);
+
+	public event EnemyDeathEffectHandler OnEnemyDeath;
+
+	void Start(){		
+		OnEnemyDeath += checkWinCondition;
+	}
+
 	public void onEnemyDeath ()
 	{
+		OnEnemyDeath (this.gameObject);
+	}
 
+	public static void checkWinCondition(GameObject enemy){
 		WaveGenerator wGenerator = GameObject.Find ("GameController").GetComponent<WaveGenerator> ();
 		wGenerator.enemyKilled ();
-
 	}
+
+
 }
